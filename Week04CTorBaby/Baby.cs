c38c79bdcc7ac79d5adc5e6ebc712e510de29afd
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-07-19 18:06:04 +0300

    Add project files.

---
+++ Week04CTorBaby/Baby.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04CTorBaby
+{
+   class Baby // Class name 
+    {
+        public string Name { get; set; } // Property for baby's name
+        public string Surname { get; set; } // Property for baby's surname
+        public DateTime DateOfBirth { get; set; } // Property for baby's date of birth
+
+        // Constructor without parameters
+        public Baby() // Default constructor
+        {
+            Name = "Sue";      // Default name
+            Surname = "Vardar";  // Default surname 
+            DateOfBirth = DateTime.Now;   // Default date of birth
+            Console.WriteLine("Ingaaaaaaa...");  // Output message when the default constructor is called
+        }
+
+        // Constructor with parameters
+        public Baby(string name, string surname) // Parameterized constructor
+        { 
+            Name = name;  // Assigning the name parameter to the Name property
+            Surname = surname;  // Assigning the surname parameter to the Surname property
+            DateOfBirth = DateTime.Now;   // Assigning the current date and time to the DateOfBirth property
+            Console.WriteLine("Ingaaaaaa...");   // Output message when the parameterized constructor is called
+        }
+
+    }
+
+}
---
+++ Week04CTorBaby/Program.cs
@@ -0,0 +1,14 @@
+﻿using Week04CTorBaby;
+using System;
+class Program // This is the main entry point of the program
+{
+    static void Main() // The Main method is the starting point of the application
+    {
+        Baby baby1 = new Baby(); // Calls the default constructor
+        Baby baby2 = new Baby("Abraham", "Lincoln"); // Calls the parameterized constructor
+        Console.WriteLine($"First baby: {baby1.Name} {baby1.Surname} - {baby1.DateOfBirth}"); // Outputs the details of the first baby
+        Console.WriteLine($"Second baby: {baby2.Name} {baby2.Surname} - {baby2.DateOfBirth}"); // Outputs the details of the second baby
+        Console.ReadLine();  // Waits for user input before closing the console window
+    }
+
+}
